f4f95471c9e9856a97164b3341f3a3dbd4755447
brendaLares12 <brendalares12@hotmail.com>
2019-12-09 16:23:34 -0300

    9/12

--- carrito.php
+++ carrito.php
@@ -18,24 +18,49 @@
   <body>
     <div class="container">
     <?php require_once 'HeadyFoot/header.php'  ?>
+    
+		<div class="row carritodecompras">	
+			<?php
+				$total=0;
+				if(isset($_SESSION['carrito'])){
+					$datos=$_SESSION['carrito'];
+					$total=0;
+					for($i=0;$i<count($datos);$i++){
+			?>
+					<div class="compra">
 
-    <div>
-      <h2>El carrito de compras está vacío</h2>
-      <p>Usted no tiene artículos en su carrito de compra.</p>
-      <p>Haga clic <a class="clic" href="index.html">aquí</a> para continuar comprando.</p>
+						<img src="../img/Muestras/<?php echo $datos[$i]['imagen'];?>" width="350" height="350"><br>
+						<span><?php echo $datos[$i]['nombre']?></span><br>
+						<span>Precio unitario: <?php echo $datos[$i]['precio']?></span><br>
+						<span>Cantidad 
+							<input type="text" value="<?php echo $datos[$i]['cantidad'];?>"
+							data-precio="<?php echo $datos[$i]['precio'];?>"
+							data-id="<?php echo $datos[$i]['id'];?>"
+							class="cantidad">
+						</span><br>
+						<span class="subtotal">Subtotal: <?php $sub = $datos[$i]['precio']*$datos[$i]['cantidad']; echo $sub ?> </span>
+
+					</div>
+				<?php
+					$total=$sub+$total;
+					}
+				}else{
+					echo '<h2>El carrito de compras está vacío</h2>';
+				}
+				echo '<h2 id="total">Total: '.$total.'</h2>';
+				if($total!=0){
+					echo'<a href="compras/compras.php">Finalizar compra</a>';
+				}
+				
+			?>
+			<br>
+			<a href="../index.php">Seguir comprando</a>
+			
+
     </div>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-            <?php require_once 'HeadyFoot/footer.php' ?>
+    
+
+    <?php require_once 'HeadyFoot/footer.php' ?>
 
     </div>
     </body>
