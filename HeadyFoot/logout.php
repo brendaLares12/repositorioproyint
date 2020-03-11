d4cf52a8ab0ba9b654954098c01cd41f0d7286bc
brendaLares12 <brendalares12@hotmail.com>
2020-03-11 15:35:37 -0300

    vacias todo

--- HeadyFoot/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
- session_start();
- session_destroy();
- setcookie('emailUsuario',null, time() -1);
- setcookie('passUsuario', null, time() -1);
- header("Location: login.php");
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-  setcookie('usuario', json_encode($usuarioFinal), time() -1);
-  header('Location: index.php');
-
-?>
