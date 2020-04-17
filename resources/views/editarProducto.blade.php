5aa2a1b56b81f138427d46a43d787e644fbcd04c
Franco Sablic <carlasablic95@gmail.com>
2020-04-16 22:53:50 -0300

    cambio en vista editar producto

--- resources/views/editarProducto.blade.php
+++ resources/views/editarProducto.blade.php
@@ -12,31 +12,21 @@
   <h2>Editar producto</h2>
   <div class="contenedor-form">
 
-  <form action="" method="post" enctype="multipart/form-data">
+  <form action="/producto/{$producto->id}" method="post" enctype="multipart/form-data">
       {{csrf_field()}}
-  <div>
-      <strong><label for="nombre">Nombre: </label></strong>
-
-  <form action="/producto/{id}" method="post" enctype="multipart/form-data">
-      {{csrf_field()}}
-  <div>
       <strong><label for="nombre">{{"Nombre"}}: </label></strong>
 
       <br>
       <input type="text" name="nombre" value="{{$producto->nombre}}">
   </div>
   <br>
-   <div>
-
-    <strong><label for="">Descripción: </label></strong>
-   </div>
+  
   <div>
    <textarea id="descripcion" name="descripcion" value="{{$producto->descripcion}}" placeholder="Escriba el contenido.."></textarea>
   </div>
   <br>
   <div>
-    <strong><label for="precio">Precio: </label></strong>
-
+  
     <strong><label for="">{{"Descripción"}}: </label></strong>
    </div>
   <div>
@@ -45,24 +35,17 @@
   <br>
   <div>
     <strong><label for="precio">{{"Precio"}}: </label></strong>
-
       <br>
       <input type="number" name="precio" step="0.01" value="{{$producto->precio}}">
      </div>
      <br>
      <div>
-
-       <strong><label for="stock">Stock: </label></strong>
-
        <strong><label for="stock">{{"Stock"}}: </label></strong>
-
          <br>
          <input type="number" name="stock" value="{{$producto->stock}}">
         </div>
         <br>
   <div>
-
-    <strong><label for="imagen">Imagen: </label></strong>
 
     <strong><label for="imagen">{{"Imagen"}}: </label></strong>
 
